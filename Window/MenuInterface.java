4ce6d2b419c16af377964c86be0ab49876989471
EmmanuelSolano <emmanuelsolano33@gmail.com>
2018-06-20 10:09:04 -0600

    Inicio

---
+++ Window/MenuInterface.java
@@ -0,0 +1,21 @@
+
+package Window;
+
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+
+
+public class MenuInterface {
+    
+    public GridPane menuWindow(){
+        GridPane window = new GridPane();
+        window.setHgap(20);
+        window.setVgap(20);
+        
+        
+        
+        return window;
+    }
+
+}
